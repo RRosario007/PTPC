87ac812712defb8c9efd09fc8b14bbb062e51548
ricar <ricar@10.0.0.106>
2023-06-14 00:43:54 -0500

    Added some JAVADOC

--- PTPC/src/main/java/calculator/Calculator.java
+++ PTPC/src/main/java/calculator/Calculator.java
@@ -3,11 +3,29 @@ package calculator;
 import java.awt.Dialog;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author ricardo
+ *
+ *	Creating a calculator. Here is where the operations are working(PEMDAS).
+ *	Need to include exponents later.
+ *	
+ */
 public class Calculator {
-	
+		
+	/**
+	 * 
+	 * @param number, the equation that we want to solve as a string
+	 * @return double, the solution to the equation as a double because of division so we don't loose decimals
+	 */
 	public double DivideNConquer(String number) {
 		number = number.replaceAll(" ", "");
 		Double total = 0.0;
+		
+		
+		/**
+		 * Using PEMDAS we do parentheses first. In here we detect parentheses and solve that first.
+		 */
 		if(number.contains("(")) {
 			for(int i = 0; i < number.length(); i++) {
 				if(number.charAt(i) == '(') {
@@ -27,7 +45,11 @@ public class Calculator {
 				}
 			}
 		}
-
+		
+		/**
+		 * After parentheses we solve multiplication and division. In this case we multiply or divide two numbers and replace the answer on the number string.
+		 * 
+		 */
 		int index = 0;
 		if(number.contains("*") || number.contains("/")) {
 			for(int i = 0; i< number.length(); i++) {
@@ -56,12 +78,22 @@ public class Calculator {
 			}
 		}
 		
+		/**
+		 * if we don't detect any other operation then we return the number
+		 */
 		if(!number.contains("-") && !number.contains("+") && !number.contains("*") && !number.contains("/") ) {
 			return Double.parseDouble(number);
 		}
+		/**
+		 * returning negative or positive number if its the last number.
+		 */
 		if((number.charAt(0) == '-' || number.charAt(0) == '+') && (!number.substring(1, number.length()).contains("-") && !number.substring(1, number.length()).contains("+"))) {		
 			return Double.parseDouble(number);
 		}
+		/**
+		 * Lastly, this is for addition and subtraction. We use recursion because with addition or subtraction the order doesn't matter as long as the signs are kept.
+		 * For example subtracting 4 is the same as adding -4. 
+		 */
 		for(int i = 1; i < number.length(); i++) {
 			if(number.charAt(i) == '+' || number.charAt(i) == '-') {
 				total = DivideNConquer(number.substring(0,i)) + DivideNConquer(number.substring(i,number.length()));
@@ -71,8 +103,7 @@ public class Calculator {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// 2 * 3 * 2 + 3 * 1 - 2 + 5 * (2 * 5 - 2 *8)	
+		// TODO Auto-generated method stub	
 		// Solution, Solve this using recursion, and I think I'm going to divide and conquer
 		// on addition and subtraction. Also pay attention to ().
 		
@@ -81,15 +112,6 @@ public class Calculator {
 		Calculator test = new Calculator();
 		
 		System.out.println(test.DivideNConquer(num));
-		
-		
-//		Scanner scan = new Scanner(System.in);
-//		System.out.println("Enter your Equation:\n");
-//		
-//		String equation = scan.nextLine();
-//		
-//		
-//		System.out.println(equation.replaceAll(" ", ""));
 
 	
 	}
