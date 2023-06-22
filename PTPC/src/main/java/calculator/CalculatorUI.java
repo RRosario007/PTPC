d1631a8daa8bcae6ad863236b185f7167096a533
ricar <ricardorosario007@gmail.com>
2023-06-22 01:01:09 -0500

    Added actionListeners and started updating textbox

--- PTPC/src/main/java/calculator/CalculatorUI.java
+++ PTPC/src/main/java/calculator/CalculatorUI.java
@@ -35,10 +35,11 @@ public class CalculatorUI implements ActionListener{
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setLayout(null);
 		mainFrame.setSize(350,450);
-		mainFrame.setVisible(true);
+		mainFrame.setLocationRelativeTo(null);
 		answerBox();
 		keypad();
 		speacialOps();
+		mainFrame.setVisible(true);
 		mainFrame.revalidate();
 	}
 	
@@ -70,13 +71,21 @@ public class CalculatorUI implements ActionListener{
 		b1 = new JButton("1");
 		b1.addActionListener(this);
 		b2 = new JButton("2");
+		b2.addActionListener(this);
 		b3 = new JButton("3");
+		b3.addActionListener(this);
 		b4 = new JButton("4");
+		b4.addActionListener(this);
 		b5 = new JButton("5");
+		b5.addActionListener(this);
 		b6 = new JButton("6");
+		b6.addActionListener(this);
 		b7 = new JButton("7");
+		b7.addActionListener(this);
 		b8 = new JButton("8");		
+		b8.addActionListener(this);
 		b9 = new JButton("9");
+		b9.addActionListener(this);
 		numberPanel.add(b7);
 		numberPanel.add(b8);
 		numberPanel.add(b9);	
@@ -88,9 +97,12 @@ public class CalculatorUI implements ActionListener{
 		numberPanel.add(b3);
 		
 		decimalButton = new JButton(".");
+		decimalButton.addActionListener(this);
 		numberPanel.add(decimalButton);
-		clearButton = new JButton("CE");					
+		clearButton = new JButton("CE");
+		clearButton.addActionListener(this);
 		b0 = new  JButton("0");
+		b0.addActionListener(this);
 		numberPanel.add(b0);
 		numberPanel.add(clearButton);
 		
@@ -105,23 +117,36 @@ public class CalculatorUI implements ActionListener{
 		operationsPanel.setBounds(225, 100, 109, 312);
 		
 		enterButton = new JButton("=");
+		enterButton.addActionListener(this);
 		operationsPanel.add(enterButton);
 		emptyButton = new JButton();
+		emptyButton.addActionListener(this);
 		operationsPanel.add(emptyButton);
 		openParen = new JButton("(");
+		openParen.addActionListener(this);
 		operationsPanel.add(openParen);
 		closeParen = new JButton(")");
+		closeParen.addActionListener(this);
 		operationsPanel.add(closeParen);
 		plusButton = new JButton("+");
+		plusButton.addActionListener(this);
 		operationsPanel.add(plusButton);
 		minusButton = new JButton("-");
+		minusButton.addActionListener(this);
 		operationsPanel.add(minusButton);
 		multButton = new JButton("*");
+		multButton.addActionListener(this);
 		operationsPanel.add(multButton);
 		divButton = new JButton("/");
+		divButton.addActionListener(this);
 		operationsPanel.add(divButton);
 		mainFrame.add(operationsPanel);
 		
+		
+	}
+	
+	private void updateText(String equation) {
+		textBox.setText(textBox.getText() + equation);
 		
 	}
 	
@@ -136,9 +161,15 @@ public class CalculatorUI implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
-		if(e.getSource() == b1) {
-			textBox.setText(textBox.getText() + "1");
+		if(e.getSource() == enterButton) {
+			System.out.println(e.getActionCommand() +  " hi");
+		}else if(e.getSource() == clearButton) {
+			System.out.println(e.getActionCommand() + " hello");
+		}else {
+			updateText(e.getActionCommand());
 		}
+		
+
 		
 	}
 }
